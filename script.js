6c8aa603781e71e258c7b08a12116cb77f5eb279
Shroff Ujjawal Sureshbhai <shroffujjawal1@gmail.com>
2024-08-28 17:34:44 +0530

    Deleted TASK-2 folder

--- script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text",{
-    strings:["Frontend Developer", "Java Developer", "Web Developer"],
-    typeSpeed : 10,
-    backSpeed : 100,
-    backDelay : 1000,
-    loop : true
-});
